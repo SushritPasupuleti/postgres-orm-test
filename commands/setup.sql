950310e722ad044a2eca5f1c9aa87ac3d421c327
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-12-15 20:38:23 +0530

    Update setup.sql

--- commands/setup.sql
+++ commands/setup.sql
@@ -4,6 +4,15 @@ CREATE TABLE person
     firstName varchar NOT NULL,
     lastname varchar NOT NULL,
     email varchar NOT NULL,
-    createdOn TIMESTAMP NOT NULL,
+    createdOn TIMESTAMP default current_timestamp,
     dob DATE NOT NULL
 );
+INSERT INTO person
+    (
+    firstName,
+    lastName,
+    email,
+    dob
+    )
+VALUES
+    ('Bruce', 'Wayne', 'bruce@wayne.com', '1988-02-22');
