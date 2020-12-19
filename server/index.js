e59086c293c2cf741d2627590525c30e9a0f4d90
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-12-19 13:34:39 +0530

    setup sequelize boilerplate for postgres

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const { Sequelize } = require('sequelize')
 const app = express()
 const port = 3000
 
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
+    host: 'localhost',
+    dialect: 'postgres'
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello There!')
+    res.send('Hello There!')
 })
 
 app.listen(port, () => {
-  console.log(`Postgres Backend listening at http://localhost:${port}`)
+    console.log(`Postgres Backend listening at http://localhost:${port}`)
 })
