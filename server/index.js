1f4b44346e324befb76ac604ac5fd3827ae6b5cd
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-12-19 13:44:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,10 +5,15 @@ const { Sequelize } = require('sequelize')
 const app = express()
 const port = 3000
 
-const sequelize = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
+const sequelize = new Sequelize(process.env.DATABASE, process.env.DBUSERNAME, process.env.DBPASSWORD, {
     host: 'localhost',
+    port: '5433', //specific to my device. leave default for rest.
     dialect: 'postgres'
 })
+
+sequelize.authenticate()
+    .then(()=> console.log("Database Connected"))
+    .catch(err => console.log("Error Connecting to DB", err))
 
 app.get('/', (req, res) => {
     res.send('Hello There!')
