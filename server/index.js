076cc071d0302f1b0de6681abcf972156c378eef
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-12-20 13:37:04 +0530

    setup Apollo Client Boilerplate

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 5000
 
 const sequelize = require('./models');
 const { QueryTypes } = require('sequelize');
--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ApolloProvider, ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:8080/v1/graphql'
+  })
+})
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      Apollo Client 🚀
     </div>
-  );
-}
+  </ApolloProvider>
+)
+
 
 export default App;
